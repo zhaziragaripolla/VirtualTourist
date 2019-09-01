8aa2860397dee61529c29455db02a846755ecc59
Zhazira <g.zhazira@gmail.com>
2019-09-01 20:58:25 +0600

    init

---
+++ VirtualTourist/PinDataService.swift
@@ -0,0 +1,74 @@
+//
+//  ViewModel.swift
+//  VirtualTourist
+//
+//  Created by Zhazira Garipolla on 8/31/19.
+//  Copyright © 2019 Zhazira Garipolla. All rights reserved.
+//
+
+import UIKit
+import Foundation
+import CoreData
+
+class PinDataService {
+    var pins = [Pin]()
+    
+    init() {
+        fetchLocations()
+    }
+    
+    private func fetchLocations() {
+        guard let appDelegate =
+            UIApplication.shared.delegate as? AppDelegate else {
+                return
+        }
+        
+        let managedContext =
+            appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest =
+            NSFetchRequest<NSManagedObject>(entityName: "Pin")
+        
+        do {
+            pins = try managedContext.fetch(fetchRequest) as! [Pin]
+        } catch let error as NSError {
+            print("Could not fetch \(error), \(error.userInfo)")
+        }
+    }
+    
+    func save(lat: Float, long: Float) {
+        
+        guard let appDelegate =
+            UIApplication.shared.delegate as? AppDelegate else {
+                return
+        }
+        
+        // 1
+        let managedContext =
+            appDelegate.persistentContainer.viewContext
+        
+        // 2
+        let entity =
+            NSEntityDescription.entity(forEntityName: "Pin",
+                                       in: managedContext)!
+        
+        let pin = NSManagedObject(entity: entity,
+                                  insertInto: managedContext)
+        
+        // 3
+        pin.setValue(lat, forKeyPath: "latitude")
+        pin.setValue(long, forKeyPath: "longitude")
+        
+        // 4
+        do {
+            try managedContext.save()
+            pins.append(pin as! Pin)
+            print("saved")
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+        catch {
+            print(error)
+        }
+    }
+}
